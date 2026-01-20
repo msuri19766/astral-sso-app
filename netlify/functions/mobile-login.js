571ecbe63b214fdba49e2585f10baf42acb9b2a6
msuri19766 <munish.suri19@gmail.com>
2026-01-20 14:15:26 -0700

    Add mobile exchange endpoint for Wix SSO

--- netlify/functions/mobile-login.js
+++ netlify/functions/mobile-login.js
@@ -1,12 +1,15 @@
 exports.handler = async () => {
   const deepLink = "astralalgomobile://auth";
 
-  const url =
-    "https://astral-sso-app.netlify.app/sso.html" +
+  // IMPORTANT: This must be your Wix site domain:
+  const WIX_SITE = "https://astralastrologer.com";
+
+  const wixBridgeUrl =
+    `${WIX_SITE}/sso-bridge` +
     `?mobile=1&redirect_uri=${encodeURIComponent(deepLink)}`;
 
   return {
     statusCode: 302,
-    headers: { Location: url },
+    headers: { Location: wixBridgeUrl },
   };
 };
